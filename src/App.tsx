c8e5d1ac55b6c23b38bdf74039a5433bc465f471
Sakthi <hsv552000@gmail.com>
2023-03-12 03:26:38 +0530

    Fix for including external js
    The 404 page requires an external js to import font awsm icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import {  Route, Routes, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import NotFound from './Component/Not Found/NotFound';
+import NotFound from "./Component/Not Found/NotFound";
 // import Home from './Component/Home/Home'
+
+/**
+ * This function includes the external library that is passed to the script tag
+ * @param {String} [externalLibraryUrl] - Holds the url that needs to be added on the script tag.
+ */
+export function AddLibrary(externalLibraryUrl: string) {
+  const script = document.createElement("script");
+  script.src = externalLibraryUrl;
+  script.async = true;
+  document.body.appendChild(script);
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route  path='/' element={<Home/>} /> */}
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route  path='/' element={<Home/>} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        {/* Call the function to add a library */}
+      </BrowserRouter>
+
+      
+      {AddLibrary(
+        "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+      )}
+    </>
   );
 }
 
--- src/Component/Not Found/NotFound.tsx
+++ src/Component/Not Found/NotFound.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './styles/style.css';
-import 'https://kit.fontawesome.com/4b9ba14b0f.js';
+
 
 function NotFound() {
   return (
