da6d95dc1530fb4db8c604d50d7251fd79547615
Sakthi <hsv552000@gmail.com>
2023-03-12 16:17:25 +0530

    Fix for external JS on Not Found page
    Included the script in the script tag in index,html

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import NotFound from "./Component/Not Found/NotFound";
 // import Home from './Component/Home/Home'
 
-/**
- * This function includes the external library that is passed to the script tag
- * @param {String} [externalLibraryUrl] - Holds the url that needs to be added on the script tag.
- */
-export function AddLibrary(externalLibraryUrl: string) {
-  const script = document.createElement("script");
-  script.src = externalLibraryUrl;
-  script.async = true;
-  document.body.appendChild(script);
-}
+
 
 function App() {
   return (
@@ -25,11 +16,6 @@ function App() {
         </Routes>
         {/* Call the function to add a library */}
       </BrowserRouter>
-
-      
-      {AddLibrary(
-        "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-      )}
     </>
   );
 }
